6fa160a12bcb03d919bcc53df6597c28b5391e97
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2020-11-22 16:25:02 -0500

    Added route for posting; added comments

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { getFileFromDir } = require("../controllers/getFileFromDir");
+const { addAnotherImageType } = require("../controllers/addAnotherImageType");
 
 router.get("/", (req, res) => {
   res.status(200).json("it works");
@@ -10,5 +11,26 @@ router.get("/favicon.ico", (req, res) => res.status(200));
 
 //route for getting image file itself from public folder
 router.get("/avatar-letter/file/:size/:letter/:fileType", getFileFromDir);
+
+/**
+ * route for posting a completely new folder for completely new images
+ * for example, adding type2 folder to public/letters
+ *
+ * @param inputFolderPath is the path where the images edited by yourself(the initial ones) are saved.
+ * For example, an inputFolderPath might equal the following: "C:/Users/ytrkp/OneDrive/avatar-letter/edited images/type1"
+ * @param outputFolderName is simply the name of the folder inside public/letters where you want to save the images.
+ * For example, outputFolderName = "type2"
+ * For example, outputFolderName = "special 3D font"
+ */
+//
+router.post(
+  "/avatar-letter/:inputFolderPath/:outputFolderName",
+  addAnotherImageType
+);
+
+// router.post(
+//   "/avatar-letter/:inputFolderPath/:outputFolderName/:size/:letter/:fileType",
+//   addAnotherImageType
+// );
 
 module.exports = router;
