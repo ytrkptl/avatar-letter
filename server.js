026a223fa263c1574246e46e01b81e2713887397
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2020-11-28 10:41:08 -0500

    added helmet

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
 const errorHandler = require("./controllers/error");
 const routes = require("./routes/index");
 const { connectMongoDB } = require("./db");
@@ -12,21 +13,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
+// added to protect against XSS and such
+// Source: https://expressjs.com/en/advanced/best-practice-security.html
+app.use(helmet());
 
-let corsOptions = {
-  origin: "*", //doesn't exist yet. this is just a dummy one.
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: "*", //doesn't exist yet. this is just a dummy one.
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+);
 
 // connect to db
 connectMongoDB();
 
 //routes
-
 app.use("/api", routes);
-//added this route to prevent adding unnecessary error logs when visiting homepage
-app.use("/", (req, res) => res.json("it works!"));
 
 // error Handler middlerware. Must keep it down here at the very end
 app.use((req, res, next) => {
